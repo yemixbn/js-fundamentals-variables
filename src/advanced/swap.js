f67558ce9d921024d9875e35e41eccfb7a5dde76
faruq <yeemirahman@gmail.com>
2024-04-17 23:10:58 +0100

    completed

--- src/advanced/swap.js
+++ src/advanced/swap.js
@@ -1,6 +1,10 @@
 const a = 8
 const b = 10
 
-// TODO: Swap the values of a and b without changing lines 1 and 2; extra points if you can do it without using a temporary variable
+const aNew = a ^ b
+const bNew = aNew ^ b
+const aFinal = aNew ^ bNew
 
-module.exports = { a, b }
+module.exports = { a: aFinal, b: bNew }
+
+console.log()
